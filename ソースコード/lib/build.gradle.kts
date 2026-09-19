2c7600eb21c49b1d323c6c371feb4bb729ce33e0
harugasumi-works <tokino.yanagi@gmail.com>
2026-09-19 20:09:42 +0900

    Fix consistency across the documents. Also update both 要件定義書 to v0.8 and システム方式設計書 to v0.3

--- ソースコード/lib/build.gradle.kts
+++ ソースコード/lib/build.gradle.kts
@@ -20,18 +20,12 @@ dependencies {
     testImplementation(libs.junit.jupiter)
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api(libs.commons.math3)
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation(libs.guava)
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(26)
     }
 }
 
